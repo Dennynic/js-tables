84601e2b177f7100da7943807e1b3afe27ed3f9a
unknown <joragdetibil@gmail.com>
2020-09-02 10:23:14 +0500

    Add some functions to dom class

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,14 +1,33 @@
-class Dom{
+class Dom {
+  constructor(selector) {
+    this.$el =
+      typeof selector === 'string'
+        ? document.querySelector(selector)
+        : selector;
+  }
+
+  html(html){
+    if(typeof html === 'string'){
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear(){
+    this.html('');
+    return this;
+  }
 }
 
-export function $() {
-  return new Dom();
+export function $(selector) {
+  return new Dom(selector);
 }
 
-$.create = (tagName, classes = '') =>{
+$.create = (tagName, classes = '') => {
   const el = document.createElement(tagName);
-  if(classes){
+  if (classes) {
     el.classList.add(classes);
   }
   return el;
-}
+};
