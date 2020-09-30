ac921ad08194bc2817ffb147c760140aee64851c
Dennynic <joragdetibil@gmail.com>
2020-09-30 07:25:03 +0500

    Add table template

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,27 +1,9 @@
-import {Component} from "@core/Component";
+import { Component } from '@core/Component';
+import { createTable } from '@/components/table/table.template';
 
-export class Table extends Component{
+export class Table extends Component {
   static className = 'excel__table';
   toHTML() {
-    return `<div class="row">
-                <div class="info">
-
-                </div>
-                <div class="data">
-                    <div class="column">A</div>
-                    <div class="column">B</div>
-                    <div class="column">C</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="info">
-                    1
-                </div>
-                <div class="data">
-                    <div class="cell selected" contenteditable></div>
-                    <div class="cell" contenteditable></div>
-                    <div class="cell" contenteditable></div>
-                </div>
-            </div>`;
+    return createTable();
   }
 }
