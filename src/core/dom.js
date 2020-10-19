3af9d85425a457972eec961fcd33d94460ae6baa
Dennynic <joragdetibil@gmail.com>
2020-10-19 10:45:26 +0500

    Add button control for cells

--- src/core/dom.js
+++ src/core/dom.js
@@ -6,25 +6,8 @@ class Dom {
         : selector;
   }
 
-  html(html) {
-    if (typeof html === 'string') {
-      this.$el.innerHTML = html;
-      return this;
-    }
-    return this.$el.outerHTML.trim();
-  }
-
-  clear() {
-    this.html('');
-    return this;
-  }
-
-  on(eventType, callback) {
-    this.$el.addEventListener(eventType, callback);
-  }
-
-  off(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback);
+  addClass(className) {
+    this.$el.classList.add(className);
   }
 
   append(node) {
@@ -40,12 +23,19 @@ class Dom {
     return this;
   }
 
+  clear() {
+    this.html('');
+    return this;
+  }
+
   closest(selector) {
     return $(this.$el.closest(selector));
   }
 
-  getCoords() {
-    return this.$el.getBoundingClientRect();
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key];
+    });
   }
 
   find(selector) {
@@ -56,18 +46,21 @@ class Dom {
     return this.$el.querySelectorAll(selector);
   }
 
-  addClass(className) {
-    this.$el.classList.add(className);
+  focus() {
+    this.$el.focus();
+    return this;
   }
 
-  removeClass(className) {
-    this.$el.classList.remove(className);
+  getCoords() {
+    return this.$el.getBoundingClientRect();
   }
 
-  css(styles = {}) {
-    Object.keys(styles).forEach(key => {
-      this.$el.style[key] = styles[key];
-    });
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
   }
 
   id(parse) {
@@ -76,6 +69,18 @@ class Dom {
       return { row: +parsed[0], col: +parsed[1] };
     }
     return this.data.id;
+  }
+
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback);
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback);
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className);
   }
 
   get data() {
